7250285aea7383163b9b9314c90106c991d1f221
Subhashre <30460898+Subhashre@users.noreply.github.com>
2017-08-02 16:19:20 +0530

    .

--- src/main/java/org/subha/program/PrintPyramidOfNumber.java
+++ src/main/java/org/subha/program/PrintPyramidOfNumber.java
@@ -2,16 +2,33 @@ package org.subha.program;
 
 public class PrintPyramidOfNumber {
 
-	public static void main(String[] arr) {
 		int rowCount = 4;
 		for (int i = 1; i <= 4; i++) {
 
 			for (int j = rowCount - i; j >= 0; j--) {
 				System.out.println(" ");
 			}
-			for(int k=i ; k>0;k--){
-				System.out.println(i+" ");
+			for (int k = i; k > 0; k--) {
+				System.out.println(i + " ");
 			}
 			System.out.println();
+
+		}
+		/////////////////////////////////////////////////////////////
+
+		for (int i = 1; i <= rowCount; i++) {
+
+			for (int j = 1; j <= rowCount - i; j++) {
+
+				System.out.print(" ");
+			}
+			int k = 1;
+			while (k <= i) {
+				System.out.print(k + " ");
+				k++;
+			}
+			System.out.println();
+		}
+
 	}
 }
