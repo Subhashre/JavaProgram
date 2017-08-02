f0eb66b3ec1c57325dad453ab6d99979cbda24d8
Subhashre <30460898+Subhashre@users.noreply.github.com>
2017-08-02 16:01:53 +0530

    Updated the code

--- src/main/java/org/subha/program/PrintPyramidOfNumber.java
+++ src/main/java/org/subha/program/PrintPyramidOfNumber.java
@@ -3,19 +3,15 @@ package org.subha.program;
 public class PrintPyramidOfNumber {
 
 	public static void main(String[] arr) {
-		int rowCount = 1;
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(" ");
+		int rowCount = 4;
+		for (int i = 1; i <= 4; i++) {
+
+			for (int j = rowCount - i; j >= 0; j--) {
+				System.out.println(" ");
 			}
-
-			for (int j = 1; j <= i; j++) {
-				System.out.print(rowCount + " ");
-
+			for(int k=i ; k>0;k--){
+				System.out.println(i+" ");
 			}
 			System.out.println();
-			rowCount++;
-
-		}
 	}
 }
